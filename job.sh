c9cc0dd01768e7b46ce33edb1d8d5f6b0677d91f
TAE JEONG KIM <monttj@gmail.com>
2018-04-11 18:18:14 +0900

    fix bug

--- job.sh
+++ job.sh
@@ -6,6 +6,6 @@ cmsRun fitMuon.py nJet2 looseiso tightid data_all data_2017 pt default
 cmsRun fitMuon.py nJet2 looseiso tightid mc_all mc_2017 pt default
 cmsRun fitMuon.py nJet3 looseiso tightid data_all data_2017 pt default
 cmsRun fitMuon.py nJet3 looseiso tightid mc_all mc_2017 pt default
-cmsRun fitMuon.py nJet4 looseiso tightid data_all data_2017 pt default
-cmsRun fitMuon.py nJet4 looseiso tightid mc_all mc_2017 pt default
+cmsRun fitMuon.py nJet4more looseiso tightid data_all data_2017 pt default
+cmsRun fitMuon.py nJet4more looseiso tightid mc_all mc_2017 pt default
 
