3dcb3defb699f4796f2417675baefcd05ace9865
TAE JEONG KIM <monttj@gmail.com>
2018-04-11 17:48:29 +0900

    multijets

--- job.sh
+++ job.sh
@@ -1,3 +1,11 @@
-cmsRun fitMuon.py test looseiso tightid data_all data_2017 pt default
-cmsRun fitMuon.py test looseiso tightid mc_all mc_2017 pt default
+cmsRun fitMuon.py nJet0 looseiso tightid data_all data_2017 pt default
+cmsRun fitMuon.py nJet0 looseiso tightid mc_all mc_2017 pt default
+cmsRun fitMuon.py nJet1 looseiso tightid data_all data_2017 pt default
+cmsRun fitMuon.py nJet1 looseiso tightid mc_all mc_2017 pt default
+cmsRun fitMuon.py nJet2 looseiso tightid data_all data_2017 pt default
+cmsRun fitMuon.py nJet2 looseiso tightid mc_all mc_2017 pt default
+cmsRun fitMuon.py nJet3 looseiso tightid data_all data_2017 pt default
+cmsRun fitMuon.py nJet3 looseiso tightid mc_all mc_2017 pt default
+cmsRun fitMuon.py nJet4 looseiso tightid data_all data_2017 pt default
+cmsRun fitMuon.py nJet4 looseiso tightid mc_all mc_2017 pt default
 
