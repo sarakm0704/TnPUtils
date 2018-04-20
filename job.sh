93beaab32d817a90739d5ec1449e09545762e8a1
TAE JEONG KIM <monttj@gmail.com>
2018-04-20 12:41:45 +0900

    clean the last line

--- job.sh
+++ job.sh
@@ -8,4 +8,3 @@ cmsRun fitMuon.py nJet3 looseiso tightid data_all data_2017 pt default
 cmsRun fitMuon.py nJet3 looseiso tightid mc_all mc_2017 pt default
 cmsRun fitMuon.py nJet4more looseiso tightid data_all data_2017 pt default
 cmsRun fitMuon.py nJet4more looseiso tightid mc_all mc_2017 pt default
-
